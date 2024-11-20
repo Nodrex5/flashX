327937e5553c177b6ba5f08e690d4c03ff663213
Mustafa22 <100078617+Nodrex5@users.noreply.github.com>
2024-11-21 01:00:56 +0300

    Update flashX.py

--- flashX.py
+++ flashX.py
@@ -4,7 +4,6 @@ from halo import Halo
 import json
 import cfonts
 
-#
 os.system('clear')
 
 print(f"""{F.CYAN}
@@ -19,52 +18,57 @@ print(f"""{F.CYAN}
                                                 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
                                                 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠋⠀⠀
 """)
-
-# 
 logo = cfonts.render(
     text="flash-X",
-    colors=["white", "cyan"],
+    colors=[
+        "white", # 
+        "cyan", #
+    ],
     align="center"
 )
 print(logo)
 
-# json ...
 with open("info.json", "r") as f:
     data = json.load(f)
     version = data["version"]
-    author = data["auther"]
+    auther = data["auther"]
 
-# ...
-print(F.RESET + '-' * 60)
+print(F.WHITE+'-'*60)
 print(f"""
-Version : {F.CYAN}{version} {F.RESET}( {F.CYAN}2024 {F.WHITE}- {F.CYAN}11 {F.WHITE}- {F.CYAN}20 {F.RESET}).
-Author  : {F.CYAN}{author}{F.RESET}.
-""")
-print(F.RESET + '-' * 60)
+Version : {F.CYAN}{version} {F.RESET}( {F.CYAN}2024 {F.WHITE}- {F.CYAN}11 {F.WHITE}- {F.CYAN}21 {F.RESET}).
+Auther  : {F.CYAN}{auther}{F.RESET}.
 
-# 
+""")
+print(F.WHITE+'-'*60)
 def listMethod():
+
     print(f'''
 {F.CYAN}1- {F.RESET}HTTP-PROXIES.
 {F.CYAN}2- {F.RESET}UDP {F.CYAN}( SOON ){F.RESET}.
-{F.CYAN}3- {F.RESET}SLOWLORIS {F.CYAN}( SOON ){F.RESET}.
+{F.CYAN}3- {F.RESET}SLOWLORIS {F.CYAN} ( SOON ){F.RESET}.
 ''')
+    
     try:
-        choiceMethod = int(input(f'''{F.RESET}┌─({F.CYAN}flash-X{F.RESET})─({F.YELLOW}~ Method{F.RESET})
+
+        choiceMethod = int(input(f'''{F.WHITE}┌─({F.CYAN}flash-X{F.WHITE})─({F.YELLOW}~ Method{F.WHITE})
 └──{F.YELLOW}~# {F.CYAN}'''))
     except ValueError:
-        print(f"{F.RED}( !!! ) Error! Invalid input.")
-        return
+        print(f"{F.RED} ( !!! ) Error !")
+    
 
     if choiceMethod == 1:
         os.system('cmd /k "python3 tools/http-flood.py"' if os.name == 'nt' else 'python3 tools/http-flood.py')
-    elif choiceMethod == 2:
-        print('SOON: UDP implementation is in progress.')
+    elif choiceMethod ==2:
+        print('soon')
+        #os.system('cmd /k "python3 tools/tsd-udp.py"' if os.name == 'nt' else 'python3 tools/tsd-udp.py')
     elif choiceMethod == 3:
-        print('SOON: SLOWLORIS implementation is in progress.')
+        print('soon')
+        #os.system('cmd /k "python3 tools/tsd-dns.py"' if os.name == 'nt' else 'python3 tools/tsd-dns.py')
+    elif choiceMethod == 4:
+        print('soon')
+        #os.system('cmd /k "python3 tools/tsd-slowloris.py"' if os.name == 'nt' else 'python3 tools/tsd-slowloris.py')
     else:
-        print(f'{F.RED}( !!! ) {F.RESET}Error! Choose {F.CYAN}1{F.RESET}, {F.CYAN}2{F.RESET}, or {F.CYAN}3{F.RESET} only.')
+        print(f'{F.RED}( !!! ) {F.RESET}Error! Choice {F.CYAN}1{F.RESET} or {F.CYAN}2{F.RESET} or {F.CYAN}3{F.RESET} just!!')
         exit()
 
-# استدعاء الوظيفة
 listMethod()
